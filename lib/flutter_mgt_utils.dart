38a1f8f10141e575849dd0c74e9b62295af78fc6
Vadim Ledyaev <vadim.ledyaev@mypatron.us>
2024-08-21 16:31:14 +0700

    chore: exports

--- lib/flutter_mgt_utils.dart
+++ lib/flutter_mgt_utils.dart
@@ -4,4 +4,6 @@ library flutter_mgt_utils;
 export 'src/async_value.dart';
 export 'src/iterable.dart';
 export 'src/painting.dart';
+export 'src/provider.dart';
 export 'src/rendering.dart';
+export 'src/view_model.dart';
